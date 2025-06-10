66eba1ffff648f6c2d98d6b975d5429cafadfd28
stroodle96 <dstrobel1@gmail.com>
2025-06-10 22:39:47 +0000

    Rename integration_blueprint to textbelt-sms across the project

--- custom_components/integration_blueprint/binary_sensor.py
+++ custom_components/integration_blueprint/binary_sensor.py
@@ -1,4 +1,4 @@
-"""Binary sensor platform for integration_blueprint."""
+"""Binary sensor platform for textbelt-sms."""
 
 from __future__ import annotations
 
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
 
 ENTITY_DESCRIPTIONS = (
     BinarySensorEntityDescription(
-        key="integration_blueprint",
+        key="textbelt-sms",
         name="Integration Blueprint Binary Sensor",
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
     ),
@@ -44,7 +44,7 @@ async def async_setup_entry(
 
 
 class IntegrationBlueprintBinarySensor(IntegrationBlueprintEntity, BinarySensorEntity):
-    """integration_blueprint binary_sensor class."""
+    """textbelt-sms binary_sensor class."""
 
     def __init__(
         self,
--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,8 +1,8 @@
-"""Constants for integration_blueprint."""
+"""Constants for textbelt-sms."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
+DOMAIN = "textbelt-sms"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
